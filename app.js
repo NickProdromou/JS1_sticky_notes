d0006ce36bae9849bc47f94df6dd0b3764a7fe84
NickProdromou <hello@nickpro.com.au>
2016-06-14 23:15:51 +1000

    simplified, variables are stil accessible in global scope, but at least it works now

--- app.js
+++ app.js
@@ -1,57 +1,33 @@
-// //variables
-//
-// var stickyNote = {
-//   noteSubmit : document.querySelector("button"),
-//   noteColour :"",
-//   createNote : function(colour, title){
-//     var createdNote = this.emptyNote;
-//     createdNote.className="box";
-//     this.noteCount ++;
-//     createdNote.innerHtml = "<h2>"+ this.noteCount + title + "</h2>";
-//     createdNote.style.background = colour;
-//     this.appendNote(createdNote);
-//   },
-//   appendNote : function(newNote){
-//     document.getElementsByClassName('container').appendChild(newNote);
-//   },
-//   emptyNote : ,
-//   noteTitle : "",
-//   noteCount : 0
-// };
-//
-// stickyNote.noteSubmit.addEventListener('click', function(){
-//   stickyNote.noteColour = document.getElementById('colour').value;
-//   stickyNote.noteTitle = document.getElementById('message').value;
-//   stickyNote.createNote(stickyNote.noteColour,stickyNote.noteTitle)
-// })
-
 //cache dom
 var submit = document.querySelector("button");
-var colourValue = document.getElementById('colour').value;
-var titleValue = document.getElementById('message').value;
 var blankNote = document.createElement('div');
-var noteCount = 0;
+var noteCount = 1;
 var newNote = createNote();
 var container = document.getElementsByClassName("container")[0];
 
 
-//function
-function createNote(title,colour){
-  var newNote = blankNote;
-  newNote.className="box";
-  noteCount += 1;
-  newNote.innerText = noteCount + "." +  title ;
-  newNote.style.background = colour;
-  return newNote;
+//event listeners
+
+submit.addEventListener('click',function(){
+  var colourField = document.querySelector('#colour');
+  var colourValue = colourField.value;
+  var titleField = document.querySelector('#message');
+  var titleValue = titleField.value;
+  var thisNote = createNote(colourValue,titleValue,noteCount);
+  appendNote(thisNote);
+})
+
+//functions
+
+function createNote(colour,title,count){
+  var currentNote = document.createElement('div');
+  currentNote.className = "box";
+  currentNote.innerHTML="<h2>" +  count + "." + "" + title + "</h2>"
+  currentNote.style.background = colour;
+  return currentNote;
 }
 
 function appendNote(note){
   container.appendChild(note);
+  noteCount ++;
 }
-
-
-//event listener
-submit.addEventListener('click', function(){
-  var createdNote = createNote(titleValue,colourValue);
-  appendNote(createdNote);
-})
